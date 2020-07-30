debbd9a398379e27d887688c580e3af92e079971
chr-ge <ace26@protonmail.com>
2020-07-29 21:49:18 -0400

    created button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 25,
+    height: 50,
+    width: 245,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  label: {
+    fontFamily: "SFProText-Regular",
+    fontSize: 15,
+    textAlign: "center",
+  },
+});
+
+interface ButtonProps {
+  variant: "default" | "primary";
+  label: string;
+  onPress: () => void;
+}
+
+const Button = ({ variant, label, onPress }: ButtonProps) => {
+  const backgroundColor =
+    variant === "primary" ? "#2CB9B0" : "rgba(12, 13, 52, 0.05)";
+  const color = variant === "primary" ? "white" : "#0C0D34";
+  return (
+    <RectButton
+      style={[styles.container, { backgroundColor }]}
+      {...{ onPress }}
+    >
+      <Text style={[styles.label, { color }]}>{label}</Text>
+    </RectButton>
+  );
+};
+
+Button.defaultProps = { variant: "default" };
+
+export default Button;
