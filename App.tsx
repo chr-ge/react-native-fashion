20d7bd7f7fd6289a3e49f4faf429da54ca3e868a
chr-ge <ace26@protonmail.com>
2020-07-30 00:39:50 -0400

    created theme file

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { ThemeProvider } from '@shopify/restyle';
 
 import { Onboarding, Welcome } from './src/Authentication';
-import { LoadAssets } from './src/components';
+import { LoadAssets, theme } from './src/components';
 
 const fonts = {
   "SFProText-Bold": require("./assets/fonts/SF-Pro-Text-Bold.otf"),
@@ -22,8 +23,10 @@ const AuthenticationNavigator = () => {
 
 export default function App() {
   return (
-    <LoadAssets {...{ fonts }}>
-      <AuthenticationNavigator />
-    </LoadAssets>
+    <ThemeProvider theme={theme}>
+      <LoadAssets {...{ fonts }}>
+        <AuthenticationNavigator />
+      </LoadAssets>
+    </ThemeProvider>
   );
 }
