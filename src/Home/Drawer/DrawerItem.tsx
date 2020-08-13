b01ab87bc6fd696e71d3f0f010566ffc72200f36
chr-ge <ace26@protonmail.com>
2020-08-13 16:15:01 -0400

    Added screen navigation to drawer

--- src/Home/Drawer/DrawerItem.tsx
+++ src/Home/Drawer/DrawerItem.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 import { Theme } from '../../components/Theme';
 import { Box, RoundIcon, Text, useTheme } from '../../components';
+import { HomeRoutes } from '../../components/Navigation';
 
 export interface DrawerItemProps {
   icon: string;
   label: string;
-  screen: string;
+  screen: keyof HomeRoutes;
   color: keyof Theme["colors"];
 }
 
 const DrawerItem = ({ icon, label, screen, color }: DrawerItemProps) => {
   const theme = useTheme();
-  
+  const { navigate } = useNavigation();
+
   return (
-    <RectButton style={{ borderRadius: theme.borderRadii.m }}>
+    <RectButton onPress={() => navigate(screen)} style={{ borderRadius: theme.borderRadii.m }}>
         <Box flexDirection="row" alignItems="center" padding="m">
           <RoundIcon 
               name={icon}
