50a736e2dd4b26c5fe7fbbbac852d0dd433f2bf6
chr-ge <ace26@protonmail.com>
2020-08-21 23:22:45 -0400

    Added TopCurve to footer of TransactionHistory

---
+++ src/Home/TransactionHistory/TopCurve.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Svg, { Defs, Path, Circle, ClipPath, Rect } from 'react-native-svg';
+import { useTheme } from '../../components';
+
+interface TopCurveProps {
+    footerHeight: number;
+}
+
+const TopCurve = ({ footerHeight }: TopCurveProps) => {
+    const theme = useTheme();
+    const SIZE = theme.borderRadii.xl;
+
+    return (
+        <Svg 
+            width={SIZE} 
+            height={SIZE}
+            style={{ 
+                position: 'absolute', 
+                bottom: footerHeight, 
+                right: 0,
+            }} 
+            viewBox="0 0 1 1"
+        >
+            <Defs>
+                <ClipPath id="clip">
+                    <Path d="M 0 1 A 0 0, 0, 0, 0, 1 0 L 1 1" fill={theme.colors.pink} />
+                </ClipPath>
+            </Defs>
+            <Rect clipPath="url(#clip)" x={0} y={0} width={1} height={1} fill={theme.colors.pink} />
+            <Circle clipPath="url(#clip)" cx={0.4} cy={0.5} r={0.5} fill={theme.colors.yellow} />
+        </Svg>
+    )
+}
+
+export default TopCurve;
--- src/Home/TransactionHistory/TransactionHistory.tsx
+++ src/Home/TransactionHistory/TransactionHistory.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
-import { Box, Header, Text } from '../../components';
+import { Image, ScrollView, StyleSheet, Dimensions } from 'react-native';
+import { Box, Header, Text, makeStyles } from '../../components';
 import { HomeNavigationProps } from '../../components/Navigation';
+import { Theme } from '../../components/Theme';
 import Graph, { DataPoint } from './Graph';
 import Transaction from './Transaction';
+import TopCurve from './TopCurve';
 
 const startDate = new Date('09/01/2019').getTime();
 const numberOfMonths = 6;
@@ -14,8 +16,22 @@ const graphData: DataPoint[] = [
     { id: 245677, date: new Date('02/01/2020').getTime(), value: 198.54, color: 'yellow' },
 ];
 
+const footerHeight = Dimensions.get('window').width / 5.5;
+const useStyles = makeStyles((theme: Theme) => ({ 
+    footer: {
+        ...StyleSheet.absoluteFillObject,
+        width: undefined,
+        height: undefined,
+        borderTopLeftRadius: theme.borderRadii.xl
+    },
+    scrollView: {
+        paddingBottom: footerHeight
+    }
+}));
+
 const TransactionHistory = ({ navigation }: HomeNavigationProps<'TransactionHistory'>) => {
-    
+    const styles = useStyles();
+
     return (
         <Box flex={1} backgroundColor="white">
             <Header 
@@ -34,11 +50,21 @@ const TransactionHistory = ({ navigation }: HomeNavigationProps<'TransactionHist
                     </Box>
                 </Box>
                 <Graph data={graphData} startDate={startDate} numberOfMonths={numberOfMonths}/>
-                <ScrollView>
-                    {
-                        graphData.map(transaction => <Transaction key={transaction.id} transaction={transaction} />)
-                    }
+                <ScrollView contentContainerStyle={styles.scrollView} showVerticalScrollIndicator={false} >
+                    {graphData.map(transaction => 
+                        <Transaction key={transaction.id} transaction={transaction} />
+                    )}
                 </ScrollView>
+            </Box>
+            <TopCurve {...{ footerHeight }}/>
+            <Box 
+                position="absolute" 
+                left={0}
+                right={0}
+                bottom={0} 
+                height={footerHeight}
+            >
+                <Image style={styles.footer} source={require('../../components/assets/1.png')} />
             </Box>
         </Box>
     )
