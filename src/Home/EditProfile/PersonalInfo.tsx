dff4ce1c33c8a3ce10613c5ae0b326efc50ce08f
chr-ge <ace26@protonmail.com>
2020-09-23 22:49:56 -0400

    Created PersonalInfo Tab for EditProfile

--- src/Home/EditProfile/PersonalInfo.tsx
+++ src/Home/EditProfile/PersonalInfo.tsx
@@ -1,13 +1,50 @@
 import React from "react";
+import { ScrollView } from "react-native";
+
 import { Box, Text, useTheme } from "../../components";
+import TextInput from "../../components/Form/TextInput";
+import CheckboxGroup from "./CheckboxGroup";
+
+const genders = [
+  { value: "male", label: "Male" },
+  { value: "female", label: "Female" },
+]
 
 const PersonalInfo = () => {
   const theme = useTheme();
-  
+
   return (
-    <Box>
-      <Text>Personal Info</Text>
-    </Box>
+    <ScrollView>
+      <Box padding="m">
+        <Text variant="body" marginBottom="m">Account Information</Text>
+        <Box marginBottom="m">
+          <TextInput
+            icon="user"
+            placeholder="Name"
+            autoCapitalize="none"
+            autoCompleteType="name"
+          />
+        </Box>
+        <Box marginBottom="m">
+          <TextInput
+            icon="lock"
+            placeholder="Enter your password"
+            autoCompleteType="password"
+            autoCapitalize="none"
+            secureTextEntry
+          />
+        </Box>
+        <Box marginBottom="m">
+          <TextInput
+            icon="map-pin"
+            placeholder="Address"
+            autoCapitalize="none"
+            autoCompleteType="street-address"
+          />
+        </Box>
+        <CheckboxGroup options={genders} radio />
+      </Box>
+    </ScrollView>
   );
 };
 
