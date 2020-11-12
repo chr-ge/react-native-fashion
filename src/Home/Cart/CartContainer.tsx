f92a0d00921db045f87c01ce309a9ec4993f79a9
chr-ge <ace26@protonmail.com>
2020-11-12 18:09:38 -0500

    Created CartContainer Component

---
+++ src/Home/Cart/CartContainer.tsx
@@ -0,0 +1,94 @@
+import React, { ReactNode } from "react";
+import { Dimensions, View } from "react-native";
+import { PanGestureHandler } from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
+import { clamp, snapPoint } from "react-native-redash";
+
+import { Box, useTheme } from "../../components";
+
+interface CartContainerProps {
+  children: ReactNode;
+}
+
+const { width } = Dimensions.get("window");
+const aspectRatio = width / 375;
+const height = 582 * aspectRatio;
+const minHeight = 228 * aspectRatio;
+const snapPoints = [-(height - minHeight), 0];
+
+const CartContainer = ({ children }: CartContainerProps) => {
+  const theme = useTheme();
+
+  const translateY = useSharedValue(0);
+  const onGestureEvent = useAnimatedGestureHandler<{ y: number }>({
+    onStart: (event, ctx) => {
+      ctx.y = translateY.value;
+    },
+    onActive: ({ translationY }, ctx) => {
+      translateY.value = clamp(
+        ctx.y + translationY,
+        snapPoints[0],
+        snapPoints[1]
+      );
+    },
+    onEnd: ({ velocityY }) => {
+      const dest = snapPoint(translateY.value, velocityY, snapPoints);
+      translateY.value = withSpring(dest, { overshootClamping: true });
+    },
+  });
+  const style = useAnimatedStyle(() => ({
+    transform: [{ translateY: translateY.value }],
+  }));
+
+  return (
+    <Box flex={1} backgroundColor="secondary">
+      <PanGestureHandler onGestureEvent={onGestureEvent}>
+        <Animated.View
+          style={[
+            {
+              position: "absolute",
+              top: 0,
+              left: 0,
+              right: 0,
+              height,
+              backgroundColor: "white",
+              borderBottomLeftRadius: theme.borderRadii.xl,
+              borderBottomRightRadius: theme.borderRadii.xl,
+            },
+            style,
+          ]}
+        >
+          {children}
+          <View
+            style={{
+              position: "absolute",
+              bottom: 0,
+              left: 0,
+              right: 0,
+              height: theme.borderRadii.xl,
+              justifyContent: "flex-end",
+              alignItems: "center",
+            }}
+          >
+            <View
+              style={{
+                height: 5 * aspectRatio,
+                backgroundColor: theme.colors.background2,
+                width: 60 * aspectRatio,
+                borderRadius: 2.5 * aspectRatio,
+                marginBottom: theme.spacing.m,
+              }}
+            />
+          </View>
+        </Animated.View>
+      </PanGestureHandler>
+    </Box>
+  );
+};
+
+export default CartContainer;
