7d0e447510eb57596d13cfecd4ec1af7052ba57b
chr-ge <ace26@protonmail.com>
2020-11-12 18:09:04 -0500

    Formatted Code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,60 +1,61 @@
-import React from 'react';
-import { View } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { Box, Text } from './Theme';
-import RoundIconButton from './RoundIconButton';
+import React from "react";
+import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Box, Text } from "./Theme";
+import RoundIconButton from "./RoundIconButton";
 
 interface HeaderProps {
-    left: {
-        icon: string;
-        onPress: () => void;
-    };
-    title: string;
-    right?: {
-        icon: string;
-        onPress: () => void;
-    };
-    dark: boolean;
+  left: {
+    icon: string;
+    onPress: () => void;
+  };
+  title: string;
+  right?: {
+    icon: string;
+    onPress: () => void;
+  };
+  dark: boolean;
 }
 
 const Header = ({ left, title, right, dark }: HeaderProps) => {
-    const insets = useSafeAreaInsets();
-    const color = dark ? 'background' : 'secondary';
-    const backgroundColor = dark ? 'secondary' : undefined;
+  const insets = useSafeAreaInsets();
+  const color = dark ? "background" : "secondary";
 
-    return (
-        <Box 
-            flexDirection="row"
-            alignItems="center"
-            justifyContent="space-between"
-            paddingHorizontal="m"
-            style={{ marginTop: insets.top }}
-        >
-            <RoundIconButton 
-                name={left.icon} 
-                iconRatio={0.4}
-                onPress={left.onPress}
-                size={44}
-                align={backgroundColor === undefined ? "flex-start" : "center"}
-                {...{ color, backgroundColor }}
-            />
-            <Text variant="header" {...{ color }}>{title.toUpperCase()}</Text>
-            {right ? (
-                <RoundIconButton 
-                    name={right.icon}  
-                    iconRatio={0.4}
-                    onPress={right.onPress}
-                    size={44}
-                    align={backgroundColor === undefined ? "flex-end" : "center"}
-                    {...{ color, backgroundColor }}
-                />
-            ) : (
-                <View style={{ width: 44 }} />
-            )}
-        </Box>
-    )
-}
+  return (
+    <Box
+      flexDirection="row"
+      alignItems="center"
+      justifyContent="space-between"
+      paddingHorizontal="m"
+      style={{ marginTop: insets.top }}
+    >
+      <RoundIconButton
+        name={left.icon}
+        iconRatio={0.4}
+        onPress={left.onPress}
+        size={44}
+        align="center"
+        {...{ color }}
+      />
+      <Text variant="header" {...{ color }}>
+        {title.toUpperCase()}
+      </Text>
+      {right ? (
+        <RoundIconButton
+          name={right.icon}
+          iconRatio={0.4}
+          onPress={right.onPress}
+          size={44}
+          align="center"
+          {...{ color }}
+        />
+      ) : (
+        <View style={{ width: 44 }} />
+      )}
+    </Box>
+  );
+};
 
-Header.defaultProps = { dark: false }
+Header.defaultProps = { dark: false };
 
 export default Header;
--- src/components/RoundIcon.tsx
+++ src/components/RoundIcon.tsx
@@ -1,38 +1,45 @@
-import React from 'react';
-import { Feather as Icon } from '@expo/vector-icons';
-import { Box, Text, Theme } from './Theme';
+import React from "react";
+import { Feather as Icon } from "@expo/vector-icons";
+import { Box, Text, Theme } from "./Theme";
 
 export interface RoundIconProps {
-    name: string;
-    size: number;
-    color: keyof Theme['colors'];
-    backgroundColor: keyof Theme['colors'] | undefined;
-    iconRatio: number;
-    align: "center" | "flex-start" | "flex-end";
+  name: string;
+  size: number;
+  color: keyof Theme["colors"];
+  backgroundColor?: keyof Theme["colors"];
+  iconRatio: number;
+  align: "center" | "flex-start" | "flex-end";
 }
 
-const RoundIcon = ({ name, size, color, backgroundColor, iconRatio, align }: RoundIconProps) => {
-    const iconSize = size * iconRatio;
-    
-    return (
-        <Box 
-            height={size} 
-            width={size} 
-            justifyContent="center"
-            alignItems={align}
-            style={{ borderRadius: size / 2 }}
-            { ...{ backgroundColor }}
-        >
-            <Text style={{ width: iconSize, height: iconSize }} {...{ color }}>
-                <Icon size={iconSize} {...{ name }}/>
-            </Text>
-        </Box>
-    )
-}
+const RoundIcon = ({
+  name,
+  size,
+  color,
+  backgroundColor,
+  iconRatio,
+  align,
+}: RoundIconProps) => {
+  const iconSize = size * iconRatio;
 
-RoundIcon.defaultProps = { 
-    iconRatio: 0.7 ,
-    align: "center"
+  return (
+    <Box
+      height={size}
+      width={size}
+      justifyContent="center"
+      alignItems={align}
+      style={{ borderRadius: size / 2 }}
+      {...{ backgroundColor }}
+    >
+      <Text style={{ width: iconSize, height: iconSize }} {...{ color }}>
+        <Icon size={iconSize} {...{ name }} />
+      </Text>
+    </Box>
+  );
+};
+
+RoundIcon.defaultProps = {
+  iconRatio: 0.7,
+  align: "center",
 };
 
 export default RoundIcon;
