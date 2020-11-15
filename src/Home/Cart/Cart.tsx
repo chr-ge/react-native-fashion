502ac090277ab234fc5e2b8a811acd6f355fa2ce
chr-ge <ace26@protonmail.com>
2020-11-15 15:56:18 -0500

    Mapping Items in an Array

--- src/Home/Cart/Cart.tsx
+++ src/Home/Cart/Cart.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Dimensions, ScrollView, StyleSheet } from "react-native";
 import Svg, { Path } from "react-native-svg";
 
@@ -12,8 +12,11 @@ const { width } = Dimensions.get("window");
 const height = 100 * aspectRatio;
 const d = "M 0 0 A 50 50 0 0 0 50 50 H 325 A 50 50 0 0 1 376 100 V 0 Z";
 
+const defaultItems = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }];
+
 const Cart = ({ navigation }: HomeNavigationProps<"Cart">) => {
   const theme = useTheme();
+  const [items, setItems] = useState(defaultItems);
 
   return (
     <CartContainer>
@@ -24,32 +27,42 @@ const Cart = ({ navigation }: HomeNavigationProps<"Cart">) => {
           left={{ icon: "arrow-left", onPress: () => navigation.goBack() }}
         />
       </Box>
-      <Box width={width} height={height}>
-        <Svg
-          style={StyleSheet.absoluteFill}
-          width={width}
-          height={height}
-          viewBox="0 0 375 100"
+      <Box flex={1}>
+        <ScrollView
+          style={{
+            borderBottomLeftRadius: theme.borderRadii.xl,
+            borderBottomRightRadius: theme.borderRadii.xl,
+          }}
+          contentContainerStyle={{ paddingVertical: 50 * aspectRatio }}
+          showsVerticalScrollIndicator={false}
         >
-          <Path d={d} fill={theme.colors.primary} />
-        </Svg>
-        <Text variant="title2" color="background" textAlign="center">
-          3 Items Added
-        </Text>
+          {items.map((item, i) => (
+            <Item
+              key={item.id}
+              onDelete={() => {
+                items.splice(i, 1);
+                setItems(items.concat());
+              }}
+            />
+          ))}
+        </ScrollView>
+        <Box
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            height,
+          }}
+        >
+          <Svg style={StyleSheet.absoluteFill} viewBox="0 0 374 100">
+            <Path d={d} fill={theme.colors.primary} />
+          </Svg>
+          <Text variant="title2" color="background" textAlign="center">
+            3 Items Added
+          </Text>
+        </Box>
       </Box>
-      <ScrollView
-        style={{
-          borderBottomLeftRadius: theme.borderRadii.xl,
-          borderBottomRightRadius: theme.borderRadii.xl,
-        }}
-        showsVerticalScrollIndicator={false}
-      >
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-      </ScrollView>
     </CartContainer>
   );
 };
