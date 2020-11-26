a6feddf71fb04a31424f456c4ee68bd902f426cb
chr-ge <ace26@protonmail.com>
2020-11-26 18:25:54 -0500

    Upgrade to Reanimated 2

--- src/Home/EditProfile/Tabs.tsx
+++ src/Home/EditProfile/Tabs.tsx
@@ -1,8 +1,8 @@
 import React, { useState, ReactNode, Children } from "react";
 import { Dimensions } from "react-native";
 import { RectButton } from "react-native-gesture-handler";
-import Animated, { multiply } from "react-native-reanimated";
-import { mix, useTransition } from "react-native-redash/lib/module/v1";
+import Animated, { useAnimatedStyle } from "react-native-reanimated";
+import { mix, useTiming } from "react-native-redash";
 import { Box, Text, useTheme } from "../../components";
 
 const { width } = Dimensions.get("window");
@@ -20,9 +20,15 @@ interface TabsProps {
 const Tabs = ({ tabs, children }: TabsProps) => {
   const theme = useTheme();
   const [index, setIndex] = useState(0);
-  const selectedTab = tabs[index];
-  const transition = useTransition(index);
-  const translateX = mix(transition, width * 0.25, width * 0.75);
+  const transition = useTiming(index);
+  const dot = useAnimatedStyle(() => ({
+    transform: [
+      { translateX: mix(transition.value, width * 0.25, width * 0.75) },
+    ],
+  }));
+  const content = useAnimatedStyle(() => ({
+    transform: [{ translateX: -width * transition.value }],
+  }));
 
   return (
     <Box flex={1}>
@@ -37,25 +43,26 @@ const Tabs = ({ tabs, children }: TabsProps) => {
           </RectButton>
         ))}
         <Animated.View
-          style={{
-            position: "absolute",
-            bottom: 0,
-            left: -5,
-            backgroundColor: theme.colors.primary,
-            width: 10,
-            height: 10,
-            borderRadius: 5,
-            transform: [{ translateX }],
-          }}
+          style={[
+            {
+              position: "absolute",
+              bottom: 0,
+              left: -5,
+              backgroundColor: theme.colors.primary,
+              width: 10,
+              height: 10,
+              borderRadius: 5,
+            },
+            dot,
+          ]}
         />
       </Box>
       <Animated.View
-        style={{
+        style={[{
           flex: 1,
           width: width * tabs.length,
           flexDirection: "row",
-          transform: [{ translateX: multiply(-width, transition) }],
-        }}
+        }, content]}
       >
         {Children.map(children, (child, i) => (
           <Box key={i} flex={1} width={width}>
